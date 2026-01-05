b3bac2d6fc1845e6ae947bb82c74634c6668c3d1
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-01-05 10:26:04 +0530

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -1,6 +1,33 @@
 public class lab1{
     public static void main(String args []){
-        int a = 3, b= 4;
-        System.out.println(a+b);
+    int a = 10, b = 5;
+
+        // Arithmetic
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+        // Assignment
+        a += 2;
+        System.out.println(a);
+
+        // Increment / Decrement
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+        // Comparison
+        System.out.println(a > b);
+        System.out.println(a == b);
+
+        // Logical
+        System.out.println(a > b && b > 0);
+
+        // Bitwise
+        System.out.println(a & b);
     }
 }
+       
+    
