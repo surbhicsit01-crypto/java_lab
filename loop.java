e722027e630c4cbd70e498272e7f5ac12811df81
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-02-03 09:46:16 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,196 @@
+// public class loop {
+//     public static void main(String args []){
+//         for(int counter =0; counter<3;counter = counter +1){
+//             System.out.println("Hello World!");
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         //counter++=> counter +1
+//         for(int counter=0; counter<11; counter++){
+//         System.out.println(counter);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         //counter++=> counter +1
+//         for(int counter=0; counter<11; counter++){
+//         System.out.print(counter+" ");
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         int i =0;
+//         while(i<11){
+//             System.out.println(i++);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         int i = 0;
+//         do { 
+//             System.out.println(i);
+//             i = i+1;
+//         } while (i<11);
+//     }
+// }
+// import java.util.*;
+// public class loop{
+//     public static void main(String args []){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int sum =0;
+//         for (int i =1;i<=n ;i++){
+//             sum = sum +i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+// import java.util.*;
+// public class loop{
+//     public static void main(String args []){
+//         Scanner sc = new Scanner(System.in);
+//             int n = sc.nextInt();
+//             for(int i=1;i<11;i++){
+//             System.out.println(n*i);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         for (int i =1; i<=5;i++){
+//             System.out.println(i);
+//         }
+
+//     }
+// }
+// public class loop{
+//     public static void main(String args[]){
+//         int i=1;
+//         while(i<=5){
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args[]){
+//         int i =1;
+//         do {
+//             System.out.println(i);
+//             i++;
+//     }while(i<=5);
+//     }
+// }
+// public class loop{
+//     public static void main(String args[]){
+//         for (int i = 10;i >= 1; i--){
+//         System.out.println(i);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main (String args []){
+//     for (int i=1;i<=10;i++){
+//     System.out.println(i);
+//     }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         for(int i = 10;i>=1;i--){
+//         System.out.println(i);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String[] args) {
+//         for(int i =0;i<=20;i+=2){
+//             System.out.println(i);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String[] args) {
+//         for(int i =1; i<=20;i+=2){
+//             System.out.println(i);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         for (int i =1;i<=10;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+// public class loop{
+//     public  static void main(String args []){
+//         for ( int i =1;i<=10;i++){
+//             System.out.println("5 x " + i +" = " + (5 * 1));
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         int n = 123;
+//         int rev = 0 ;
+//         while(n != 0){
+//             rev = rev * 10 + n % 10;
+//             n = n/10;
+//         }
+//         System.out.println(rev);
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         int n = 12345;
+//         int count =0;
+//         n = n/10;
+//         while(n != 0){
+//             count++;
+//             n =n/10;
+//         }
+//         System.out.println(count);
+//     }
+// }
+// public class loop{
+//     public void main(String args []){
+//         int fact = 0;
+//         int n =5;
+//     for  (int i = 1; i<=n; i++){
+//         fact *=i;
+//     }
+//     System.out.println(fact);
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         for (int i =1;i <=30;i++){
+//             if(i % 3==0)
+//             System.out.println(i);
+//         }
+//     }
+// }
+// public class loop{
+//     public static void main(String args []){
+//         int i =1;
+//         while(i <11){
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+public class loop{
+    public static void main(String args []){
+        int i=1;
+        do { 
+        System.out.println(i);
+        i ++;
+    }while(i<11);
+}
+}
