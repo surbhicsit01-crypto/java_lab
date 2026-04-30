3d3df21c17ea4625bb7b2312d7f6b3790ef3337f
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-30 21:51:03 +0530

    Add files via upload

--- ques1.java
+++ ques1.java
@@ -1,51 +1,22 @@
 import java.util.Scanner;
 
-class Box {
-    double width, height, depth;
-
-    Box(double w, double h, double d) {
-        width = w;
-        height = h;
-        depth = d;
-    }
-
-    double volume() {
-        return width * height * depth;
-    }
-}
-
-class BoxWeight extends Box {
-    double costPerKm;
-
-    BoxWeight(double w, double h, double d, double cost) {
-        super(w, h, d);
-        costPerKm = cost;
-    }
-
-    double shippingCost(double distance) {
-        return distance * volume() * costPerKm;
-    }
-}
-
 public class ques1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter width height depth: ");
-        double w = sc.nextDouble();
-        double h = sc.nextDouble();
-        double d = sc.nextDouble();
+        // Uppercase to lowercase
+        System.out.print("Enter uppercase string: ");
+        String upper = sc.nextLine();
+        System.out.println("Lowercase: " + upper.toLowerCase());
 
-        System.out.print("Enter cost per km: ");
-        double cost = sc.nextDouble();
+        // Lowercase to uppercase
+        System.out.print("Enter lowercase string: ");
+        String lower = sc.nextLine();
+        System.out.println("Uppercase: " + lower.toUpperCase());
 
-        System.out.print("Enter distance: ");
-        double distance = sc.nextDouble();
-
-        Box obj = new BoxWeight(w, h, d, cost);
-
-        BoxWeight bw = (BoxWeight) obj;
-        System.out.println("Volume of Box: " + bw.volume());
-        System.out.println("Shipping Cost: " + bw.shippingCost(distance));
+        // Remove spaces from sentence
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+        System.out.println("Without spaces: " + sentence.replace(" ", ""));
     }
 }
