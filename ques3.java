24bcc10687a76b3fc492f75092e1b26737affab8
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-30 21:39:38 +0530

    Add files via upload

--- ques3.java
+++ ques3.java
@@ -1,22 +1,38 @@
-class ques3 {
+abstract class Shape {
+    abstract double findArea();
+}
+
+class Square extends Shape {
+    double side;
+
+    Square(double s) {
+        side = s;
+    }
+
+    double findArea() {
+        return side * side;
+    }
+}
+
+class Rectangle extends Shape {
+    double length, breadth;
+
+    Rectangle(double l, double b) {
+        length = l;
+        breadth = b;
+    }
+
+    double findArea() {
+        return length * breadth;
+    }
+}
+
+public class ques3 {
     public static void main(String[] args) {
-        String[][] booking = new String[1][4];
+        Shape s1 = new Square(5);
+        Shape s2 = new Rectangle(4, 6);
 
-        booking[0][0] = "10-02-2026";  // Date
-        booking[0][1] = "10AM";        // Start time
-        booking[0][2] = "12PM";        // End time
-        booking[0][3] = "Surbhi";     // Name
-
-        String date = "10-02-2026";
-        String start = "10AM";
-        String end = "12PM";
-
-        if (booking[0][0].equals(date) &&
-            booking[0][1].equals(start) &&
-            booking[0][2].equals(end)) {
-            System.out.println("Room already booked by " + booking[0][3]);
-        } else {
-            System.out.println("Room is available");
-        }
+        System.out.println("Area of Square: " + s1.findArea());
+        System.out.println("Area of Rectangle: " + s2.findArea());
     }
 }
