f5dc4cc8a3058b04b11c48c2d6d6a265973f310d
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-30 21:38:48 +0530

    Add files via upload

--- ques1.java
+++ ques1.java
@@ -1,23 +1,51 @@
-class A {
-    A() {
-        System.out.println("Constructor of A");
+import java.util.Scanner;
+
+class Box {
+    double width, height, depth;
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    double volume() {
+        return width * height * depth;
     }
 }
 
-class B extends A {
-    B() {
-        System.out.println("Constructor of B");
+class BoxWeight extends Box {
+    double costPerKm;
+
+    BoxWeight(double w, double h, double d, double cost) {
+        super(w, h, d);
+        costPerKm = cost;
+    }
+
+    double shippingCost(double distance) {
+        return distance * volume() * costPerKm;
     }
 }
 
-class C extends B {
-    C() {
-        System.out.println("Constructor of C");
-    }
-}
-
-class ques1        {
+public class ques1 {
     public static void main(String[] args) {
-        C obj = new C();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter width height depth: ");
+        double w = sc.nextDouble();
+        double h = sc.nextDouble();
+        double d = sc.nextDouble();
+
+        System.out.print("Enter cost per km: ");
+        double cost = sc.nextDouble();
+
+        System.out.print("Enter distance: ");
+        double distance = sc.nextDouble();
+
+        Box obj = new BoxWeight(w, h, d, cost);
+
+        BoxWeight bw = (BoxWeight) obj;
+        System.out.println("Volume of Box: " + bw.volume());
+        System.out.println("Shipping Cost: " + bw.shippingCost(distance));
     }
 }
