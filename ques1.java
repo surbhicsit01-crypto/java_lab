0bd315b3776a2ea38898bbb6a7b438148bd2f39b
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-04-30 15:03:01 +0530

    Add files via upload

--- ques1.java
+++ ques1.java
@@ -1,19 +1,23 @@
-class ques1 {
+class A {
+    A() {
+        System.out.println("Constructor of A");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("Constructor of B");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("Constructor of C");
+    }
+}
+
+class ques1        {
     public static void main(String[] args) {
-        String[] stocks = {"TATA", "INFY"};
-        int[][] prices = {
-            {100, 105, 110},   // TATA prices
-            {200, 210, 215}    // INFY prices
-        };
-
-        String[] time = {"10AM", "12PM", "2PM"};
-
-        for (int i = 0; i < stocks.length; i++) {
-            System.out.println("Stock: " + stocks[i]);
-            for (int j = 0; j < time.length; j++) {
-                System.out.println(time[j] + " : " + prices[i][j]);
-            }
-            System.out.println();
-        }
+        C obj = new C();
     }
 }
