b48414e9b4398f509dd894fb468ad987d9264dc8
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-05-01 12:33:57 +0530

    Add files via upload

---
+++ ImportDemo.java
@@ -0,0 +1,7 @@
+import static java.lang.Math.*;     // single class import
+
+class ImportDemo {
+    public static void main(String[] args) {
+        System.out.println("Square root of 25: " + sqrt(25));
+    }
+}
