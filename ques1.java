9313ee3aff445ad27a0674dc520c141aeffc2a59
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-05-02 11:54:28 +0530

    Add files via upload

--- ques1.java
+++ ques1.java
@@ -1,16 +1,3 @@
-import java.util.Scanner;
-
 public class ques1 {
-    public static void main(String[] args) {
-        int[] arr = {10, 20, 30, 40, 50};
-        Scanner sc = new Scanner(System.in);
-
-        try {
-            System.out.print("Enter index (0-4): ");
-            int index = sc.nextInt();
-            System.out.println("Element at index " + index + " is: " + arr[index]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Error: Invalid index entered!");
-        }
-    }
+    
 }
